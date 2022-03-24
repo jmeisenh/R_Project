7a8945edce74e782ba0e010ac6e0f5dc06adde56
Justin Meisenhelter <jmeisenh@yahoo.com>
2022-03-24 10:08:13 -0400

    Finished first draft

--- Life_Expectancy/server.R
+++ Life_Expectancy/server.R
@@ -16,14 +16,14 @@ function(input, output){
   output$BMIplot <- renderPlot(
     BMIData() %>%
       ggplot(., aes(x = BMI, y = Life_Expectancy, color = Type)) + geom_smooth()
-    + ggtitle(paste0('Life Expectancy vs. BMI for Peoples Educated ',(input$minSchool + input$maxSchool)/2, ' Years')) +
+    + ggtitle(paste0('Life Expectancy vs. BMI')) +
       labs(y = 'Life Expectancy', color = 'Fit\n') + theme(plot.title = element_text(size = 20, face = 'bold')) + 
       scale_color_discrete(labels = c('Predicted', 'Lower Confidence Band', 'Upper Confidence Band', 'Lower Prediction Band', 'Upper Prediction Band'))
   )
   output$SchoolPlot <- renderPlot(
     SchoolData() %>%
       ggplot(., aes(x = Schooling, y = Life_Expectancy, color = Type)) + geom_smooth() +
-      ggtitle(paste0('Life Expectancy vs. Years Educated for Peoples with a BMI of ', (input$minBMI + input$maxBMI)/2)) +
+      ggtitle(paste0('Life Expectancy vs. Years Educated')) +
       labs(y = 'Life Expectancy', x = 'Years Educated', color = 'Fit\n') + theme(plot.title = element_text(size = 20, face = 'bold')) + 
       scale_color_discrete(labels = c('Predicted', 'Lower Confidence Band', 'Upper Confidence Band', 'Lower Prediction Band', 'Upper Prediction Band'))
   )
